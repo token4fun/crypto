4469f003fb304ebb3360de9672b81d19d3338099
Token4Fun <token4fun@gmail.com>
2025-01-12 13:11:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript for future functionality
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Welcome to Token4Fun! Your website and music video creation hub.');
+    // You can add more interactive features here
+});
